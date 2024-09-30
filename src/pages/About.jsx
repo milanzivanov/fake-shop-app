94e34b0e3eb16d28c3e0b571a4ac5103fcbd62fe
milanzivanov <milan.zivanov@gmail.com>
2024-09-30 20:22:16 +0200

    center image in productDetails component

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -12,19 +12,19 @@ function Product() {
         <h3 className="text-2xl font-bold tracking-wide py-2">
           Final task from javascript-react course summer 2024
         </h3>
-        <li className="text-balance py-2">
+        <li className="text-balance py-5 border-b">
           Napraviti aplikaciju za online narucivanje garderobe. Aplikacija ce
           imati 5 kljucnih stranica kako bismo omogucili jednostavno I ugodno
           iskustvo korisnicima, te stranice su sledece: Home, Products, Product,
           Cart I Order Summary stranice. U zaglavlju aplikacije treba da budu
           prikazane samo prve 4 (Home, Products, Product, Cart).
         </li>
-        <li className="text-balance py-2">
+        <li className="text-balance py-5 border-b">
           Pocinjemo sa Home stranicom, gde smo kreativni u dizajniranju izgleda.
           Na ovoj stranici isticemo dugme Discover our products koje vodi
           korisnike direktno na Products stranicu.
         </li>
-        <li className="text-balance py-2">
+        <li className="text-balance py-5 border-b">
           Products stranica prikazuje proizvode dobijene putem odredjenog API-a
           koje mozete naci na kraju zadatka. Svaki proizvod treba da ima
           prikazanu sliku, naslov proizvoda, cenu. Ovi proizvodi predstavljaju
@@ -35,18 +35,18 @@ function Product() {
           kategorija, cena I dugme Add to Cart koje omogucava korisnicima da ih
           dodaju u korpu.
         </li>
-        <li className="text-balance py-2">
+        <li className="text-balance py-5 border-b">
           Na Cart stranici korisnici mogu pregledati proizvode koje su dodali u
           korpu. Klikom na dugme Go to checkout otvara se Order Summary
           stranica.
         </li>
-        <li className="text-balance py-2">
+        <li className="text-balance py-5 border-b">
           Order Summary stranica sadrzi formu za popunjavanje adrese za
           dostavu(ime, prezime, adresa, grad I email). Pored toga, pruza pregled
           svih dodatih proizvoda I ukupnu cenu. U okviru forme, postoji dugme
           Checkout koje, kada se klikne, vraca korisnike na Home stranicu.
         </li>
-        <li className="py-2 mb-10 text-balance text-indigo-500" target="_blank">
+        <li className="py-5 mb-10 text-balance text-indigo-500" target="_blank">
           API: <a href="https://fakestoreapi.com">https://fakestoreapi.com</a>/
         </li>
         <button
--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -15,8 +15,6 @@ function ProductDetails() {
   const product = useLoaderData();
   const { addToCart } = useContext(CartContext);
 
-  console.log(product);
-
   function handleAddToCart() {
     addToCart(product);
   }
@@ -25,24 +23,28 @@ function ProductDetails() {
     <div>
       <div className="mx-auto max-w-7xl">
         <div className="flex justify-center py-5">
-          <div className="flex justify-center flex-col items-center w-1/2 bg-slate-50 shadow-md mt-5 p-5">
+          <div className="flex flex-col items-start w-1/2 bg-slate-50 shadow-md mt-5 p-5">
             <h2 className="text-center tracking-wide text-3xl py-5 font-bold text-gray-900">
               {product.title}
             </h2>
-            <img
-              className=" w-1/2 pb-5"
-              src={product.image}
-              alt={product.title}
-            />
+            <div className="flex justify-center">
+              <img
+                className="w-1/2 pb-5"
+                src={product.image}
+                alt={product.title}
+              />
+            </div>
             <p className=" text-pretty leading-relaxed pb-5">
               {product.description}
             </p>
-            <p className="text-md text-balanced pb-5">
-              <span className="font-bold">Category</span>: {product.category}
-            </p>
-            <p className="text-xl italic pb-5">
-              <span className="font-bold">Price</span>:{product.price}&#8364;
-            </p>
+            <div className="flex flex-col">
+              <p className="text-md text-balanced pb-5">
+                <span className="font-bold">Category</span>: {product.category}
+              </p>
+              <p className="text-xl italic pb-5">
+                <span className="font-bold">Price</span>:{product.price}&#8364;
+              </p>
+            </div>
             <div className="flex items-center gap-5">
               <button
                 className="text-white bg-indigo-500 rounded-sm px-3 py-2 hover:bg-indigo-600"
